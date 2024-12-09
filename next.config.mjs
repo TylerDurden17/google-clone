7a6d74cdde82c835fd5f2cce189506de6bbfa03b
TylerDurden17 <kartikrai14@gmail.com>
2024-12-09 06:49:15 +0530

    meow

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['ssl.gstatic.com', 'cataas.com', 'www.google.com', 'picsum.photos', 'lh3.googleusercontent.com'],
+  },
+};
+
+export default nextConfig;
